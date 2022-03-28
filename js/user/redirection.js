aa592fc73415e4804d56564419d1da13a886104d
Sebastián Yactayo <syactayocrisostomo@gmail.com>
2022-03-27 22:01:10 -0500

    [UPDATE] changes in fetch

--- js/user/redirection.js
+++ js/user/redirection.js
@@ -1,5 +1,5 @@
 if (window.location.href.includes("index")) {
   setTimeout(() => {
-    window.location.href = "./user/home.html";
+    window.location.href = "./public/user/home.html";
   }, 2000);
 }
