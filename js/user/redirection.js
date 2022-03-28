c3da539dc49c1777e91778a64d03d39c8d59fc97
Sebastián Yactayo <syactayocrisostomo@gmail.com>
2022-03-27 22:27:17 -0500

    [UPDATE] redirection

--- js/user/redirection.js
+++ js/user/redirection.js
@@ -1,5 +1,3 @@
-if (window.location.href.includes("index")) {
-  setTimeout(() => {
-    window.location.href = "./public/user/home.html";
-  }, 2000);
-}
+setTimeout(() => {
+  window.location.href = "./public/user/home.html";
+}, 2000);
